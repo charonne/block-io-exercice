21534524517473f969abd70aa2d6923309a4df4e
Phoax <raphael.pralat@gmail.com>
2016-11-11 16:18:42 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+include('block_io.php');
+
+$apiKey = 'xxxxxx';
+$version = 2; // API version
+$pin = 'xxxxxx';
+$block_io = new BlockIo($apiKey, $pin, $version);
+
+$balance = $block_io->get_address_balance(array('addresses' => '2N6rmc2rcsFnPQHPskzVRGQ9XyHbEmyPudX'));
+var_dump($balance);
